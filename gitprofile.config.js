00c47eaf50f9b3b7e499a804f550bd133b5bf0e6
TarikFERTAHI <fertahitarik@gmail.com>
2022-10-17 23:25:59 -0700

    Updated theme

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -176,7 +176,7 @@ const config = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'bumblebee',
+    defaultTheme: 'light',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
